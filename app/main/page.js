859c93cc66a33d6740c4120e71c4d7deedab435e
kritigaba710 <kritigaba31@gmail.com>
2025-04-25 06:56:01 -0700

    fixing routes

--- app/main/page.js
+++ app/main/page.js
@@ -28,7 +28,7 @@ export default function MainDashboard() {
   return (
     <main className="min-h-screen bg-gradient-to-br pt-5 from-[#ddb27b] via-[#c29073] to-[#dfb288]">
       <Navbar />
-      <div className="mt-8 mx-4 rounded-4xl font-bold mb-6 text-center shadow-lg overflow-hidden relative h-[30vh] sm:h-[35vh] md:h-[40vh] lg:h-[45vh] p-8 sm:pt-12 md:p-20 lg:p-24 text-3xl sm:text-4xl md:text-6xl lg:text-7xl bg-[url('/mainbg.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="mt-8 mx-4 rounded-4xl font-bold mb-6 text-center shadow-lg overflow-hidden relative h-[30vh] sm:h-[35vh] md:h-[40vh] lg:h-[45vh] p-8 sm:pt-12 md:p-20 lg:p-24 text-3xl sm:text-4xl md:text-6xl lg:text-7xl bg-[url('public/mainbg.jpg')] bg-cover bg-center bg-no-repeat">
         <span className="shiny-text block w-full my-auto text-white drop-shadow-md">
           Welcome, {session.user?.name}!
         </span>
@@ -43,7 +43,7 @@ export default function MainDashboard() {
           <h2 className="font-bold text-4xl mb-4">💧 Water Tracker</h2>
           <p className="text-lg font-semibold">Track your daily hydration goal and glasses consumed.</p>
           <button
-            onClick={() => router.push("/water")}
+            onClick={() => router.push("app/water/page.js")}
             className="mt-4 cursor-pointer bg-blue-500 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
             Go to Water Page
@@ -58,7 +58,7 @@ export default function MainDashboard() {
           <h2 className="text-4xl font-bold mb-4">🏋️ Workouts</h2>
           <p className="text-lg font-semibold">Log workouts, learn new ones, and track calories burned & eaten.</p>
           <button
-            onClick={() => router.push("/workout")}
+            onClick={() => router.push("app/workout/page.js")}
             className="mt-4 cursor-pointer text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
             Go to Workout Page
